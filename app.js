afa9a5cbb8b1a419b32232c1c736f2010d858de9
Aram Hekimian <aramhek121@gmail.com>
2023-02-28 02:59:27 -0600

    Finished main design and logic

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 (function () {
   const page = {
     uInput: "",
+    name: "",
+    time: "",
+    img: "",
+    tempK: "",
+    tempC: "",
+    tempF: "",
+    weath: "",
+    regionNames: new Intl.DisplayNames(["en"], { type: "region" }),
     init() {
       this.cacheDom();
       this.bindEvents();
@@ -9,23 +17,55 @@
     cacheDom() {
       this.searchBtn = document.getElementById("searchIcon");
       this.searchInput = document.getElementById("searchInput");
+      this.cityName = document.getElementById("cityName");
+      this.cityTime = document.getElementById("cityTime");
+      this.cityImg = document.getElementById("cityImg");
+      this.cityTemp = document.getElementById("cityTemp");
+      this.cityWeath = document.getElementById("cityWeath");
     },
     bindEvents() {
       document.addEventListener("keydown", (e) =>
         e.key === "Enter" && this.searchInput.className !== "searched"
-          ? this.submitSearch()
+          ? this.searchAndRender()
           : null
       );
-      this.searchBtn.addEventListener("click", this.submitSearch.bind(this));
+      this.searchBtn.addEventListener("click", this.searchAndRender.bind(this));
     },
     async getWeatherData() {
-      console.log(`Looking up ${this.uInput}`);
-      const data = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${this.uInput}&APPID=f70b1477a53fdaa02a713d62054b16bf`,
-        { mode: "cors" }
-      );
-      const dataJSON = await data.json();
-      console.log(dataJSON);
+      try {
+        const data = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${this.uInput}&APPID=f70b1477a53fdaa02a713d62054b16bf`,
+          { mode: "cors" }
+        );
+        const dataJSON = await data.json();
+        console.log(dataJSON);
+        this.name = `${dataJSON.name}, ${this.regionNames.of(
+          dataJSON.sys.country
+        )}`;
+        const d = new Date();
+        const localTime = d.getTime();
+        const localOffset = d.getTimezoneOffset() * 60000;
+        const utc = localTime + localOffset;
+        const tz = utc + 1000 * dataJSON.timezone;
+        const nd = new Date(tz);
+        console.log(this.name);
+        this.time = nd.toLocaleString(undefined, {
+          day: "2-digit",
+          weekday: "short",
+          month: "short",
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        console.log(this.time);
+        this.tempK = Math.floor(dataJSON.main.temp);
+        this.tempC = Math.floor(this.tempK - 273.15);
+        this.tempF = Math.floor(this.tempC * 1.8 + 32);
+        console.log(this.tempK, this.tempC, this.tempF);
+        this.weath = dataJSON.weather[0].main;
+        console.log(this.weath);
+      } catch (err) {
+        alert(err);
+      }
     },
     hideShowInput() {
       document.activeElement.blur();
@@ -36,14 +76,22 @@
     captureUserInput() {
       this.uInput = this.searchInput.value;
     },
-    submitSearch() {
-      if (this.searchInput.className !== "searched") {
+    async searchAndRender() {
+      if (
+        this.searchInput.className !== "searched" &&
+        this.searchInput.value !== ""
+      ) {
         this.captureUserInput();
         this.hideShowInput();
-        this.getWeatherData();
-      } else {
-        this.hideShowInput();
-      }
+        await this.getWeatherData();
+        this.render();
+      } else this.hideShowInput();
+    },
+    render() {
+      this.cityName.innerText = this.name;
+      this.cityTime.innerText = this.time;
+      this.cityTemp.innerText = this.tempC;
+      this.cityWeath.innerText = this.weath;
     },
   };
   page.init();
