c8ee279a2125803a77fd42ce208051291bc8cfb9
Aram Hekimian <aramhek121@gmail.com>
2023-02-28 04:06:06 -0600

    Added geolocation API

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// API Key f70b1477a53fdaa02a713d62054b16bf
+// eslint-disable-next-line func-names
 (function () {
   const page = {
     uInput: "",
@@ -9,10 +9,15 @@
     tempC: "",
     tempF: "",
     weath: "",
+    uLong: "",
+    uLat: "",
     regionNames: new Intl.DisplayNames(["en"], { type: "region" }),
-    init() {
+    async init() {
       this.cacheDom();
       this.bindEvents();
+      await this.getLocation();
+      await this.getWeatherData();
+      this.render();
     },
     cacheDom() {
       this.searchBtn = document.getElementById("searchIcon");
@@ -22,6 +27,8 @@
       this.cityImg = document.getElementById("cityImg");
       this.cityTemp = document.getElementById("cityTemp");
       this.cityWeath = document.getElementById("cityWeath");
+      this.degree = document.getElementById("degree");
+      this.degreeBtn = document.getElementById("degreeBtn");
     },
     bindEvents() {
       document.addEventListener("keydown", (e) =>
@@ -30,15 +37,24 @@
           : null
       );
       this.searchBtn.addEventListener("click", this.searchAndRender.bind(this));
+      this.degreeBtn.addEventListener("click", this.changeDegree.bind(this));
     },
     async getWeatherData() {
       try {
+        if (this.uInput === "") {
+          const response1 = await fetch(
+            `http://api.openweathermap.org/geo/1.0/reverse?lat=${this.uLat}&lon=${this.uLong}&appid=f70b1477a53fdaa02a713d62054b16bf`,
+            { mode: "cors" }
+          );
+          const response2 = await response1.json();
+          this.uInput = response2[0].state;
+        }
         const data = await fetch(
           `https://api.openweathermap.org/data/2.5/weather?q=${this.uInput}&APPID=f70b1477a53fdaa02a713d62054b16bf`,
           { mode: "cors" }
         );
+
         const dataJSON = await data.json();
-        console.log(dataJSON);
         this.name = `${dataJSON.name}, ${this.regionNames.of(
           dataJSON.sys.country
         )}`;
@@ -48,7 +64,6 @@
         const utc = localTime + localOffset;
         const tz = utc + 1000 * dataJSON.timezone;
         const nd = new Date(tz);
-        console.log(this.name);
         this.time = nd.toLocaleString(undefined, {
           day: "2-digit",
           weekday: "short",
@@ -56,14 +71,12 @@
           hour: "2-digit",
           minute: "2-digit",
         });
-        console.log(this.time);
         this.tempK = Math.floor(dataJSON.main.temp);
         this.tempC = Math.floor(this.tempK - 273.15);
         this.tempF = Math.floor(this.tempC * 1.8 + 32);
-        console.log(this.tempK, this.tempC, this.tempF);
         this.weath = dataJSON.weather[0].main;
-        console.log(this.weath);
       } catch (err) {
+        // eslint-disable-next-line no-alert
         alert(err);
       }
     },
@@ -75,6 +88,35 @@
     },
     captureUserInput() {
       this.uInput = this.searchInput.value;
+    },
+    changeDegree() {
+      if (this.degree.className === "C") {
+        this.degree.classList.toggle("C");
+        this.degree.classList.toggle("F");
+        this.cityTemp.innerText = this.tempF;
+        this.degree.innerText = "°F";
+        this.degreeBtn.innerText = "°C";
+      } else if (this.degree.className === "F") {
+        this.degree.classList.toggle("F");
+        this.degree.classList.toggle("C");
+        this.cityTemp.innerText = this.tempC;
+        this.degree.innerText = "°C";
+        this.degreeBtn.innerText = "°F";
+      }
+    },
+    getLocationPromise() {
+      return new Promise((resolve, reject) => {
+        // Promisifying the geolocation API
+        navigator.geolocation.getCurrentPosition(
+          (position) => resolve(position),
+          (error) => reject(error)
+        );
+      });
+    },
+    async getLocation() {
+      const data = await this.getLocationPromise();
+      this.uLat = data.coords.latitude;
+      this.uLong = data.coords.longitude;
     },
     async searchAndRender() {
       if (
